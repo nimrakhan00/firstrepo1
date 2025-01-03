ca1a4d19d0afb9628c16ff79187f662b93ccae7c
Aaaqib RajpooT <73097636+aaqib-rajpoot@users.noreply.github.com>
2025-01-03 15:13:51 +0500

    Update project.py
    added stock class again

--- Project/project.py
+++ Project/project.py
@@ -58,4 +58,19 @@ class Exams(School):
     pass
   
 class Stock(School):
-    pass
+    def __init__(self, name, quantity, price_per_item, category, supplier):
+        self.name = name
+        self.quantity = quantity
+        self.price_per_item = price_per_item
+        self.category = category 
+        self.supplier = supplier
+    def get(self):
+        total_value = self.quantity * self.price_per_item
+        return {
+            "name": self.name,
+            "quantity": self.quantity,
+            "price_per_item": self.price_per_item,
+            "total_value": total_value,
+            "category" : self.category,
+            "supplier": self.supplier
+        }
