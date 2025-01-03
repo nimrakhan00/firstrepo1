0bffea45cea07d4b80a6c1a9f6d5dc2506924f1f
nimrakhan <nk1304567@gmail.com>
2025-01-03 07:14:24 -0800

    Update project.py
    added class Exam by nimra

--- Project/project.py
+++ Project/project.py
@@ -66,7 +66,20 @@ class Event(School):
         self.description = description
 
 class Exams(School):
-    pass
+    def __init__(self, student_name, student_rollNumber, obtain_marks, total_marks, subject, date):
+        self.student_name = student_name
+        self.student_rollNumber = student_rollNumber
+        self.obtain_marks = obtain_marks  
+        self.total_marks = total_marks 
+        self.subject = subject
+        self.date = date
+        
+        self.percentage = (self.obtain_marks / self.total_marks) * 100  
+
+    def get_exam(self):
+        return f"Student Name: {self.student_name},\nStudent Roll Number: {self.student_rollNumber},\nObtain Marks: {self.obtain_marks},\nTotal Marks: {self.total_marks},\nSubject: {self.subject},\nDate: {self.date},\nPercentage: {self.percentage:.2f}%"
+        
+
   
 class Stock(School):
     def __init__(self, name, quantity, price_per_item, category, supplier):
